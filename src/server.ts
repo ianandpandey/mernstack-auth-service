a0726cfc59f0c1fe1b8ce88d5cea94f117c44cc3
ianandpandey <ianandpandey99@gmail.com>
2023-10-04 22:13:21 +0530

    [add]: express js

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import app from './app';
+import { Config } from './config';
+
+const startServer = () => {
+  const PORT = Config.PORT;
+
+  try {
+    // eslint-disable-next-line no-console
+    app.listen(PORT, () => console.log(`listen on PORT ${PORT}`));
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+startServer();
